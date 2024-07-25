9b81b35e4b9d711dfea9f119e40906a818b9cffb
Emy Santo <emysanto4444@gmail.com>
2024-07-25 19:18:27 +0530

    Create struct_array.cpp

---
+++ struct_array.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+struct new1{
+int a;
+string b;
+};
+int main()
+{new1 ar[3];
+ for(int i=0;i<3;i++)
+ {
+     cin>>ar[i].a;
+     cin>>ar[i].b;
+ }
+ for(int i=0;i<3;i++)
+ {
+     cout<<"element "<<i+1<<"ID :"<<ar[i].a<<" Name="<<ar[i].b<<endl;
+ }
+    return 0;
+}
